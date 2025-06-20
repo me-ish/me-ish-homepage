18527dc4cb7362a10aadac2f18b9a652c24ae567
me-ish <info@me-ish.art>
2025-06-20 23:59:02 +0900

    fix: hardcode redirectTo to verify production issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
+        redirectTo: 'https://www.me-ish.art/auth/callback'
       },
     });
   };
